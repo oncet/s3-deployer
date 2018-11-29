4a4a0cb108ddedbf5522414dfa47c3601ac10fac
Camilo <camilo@gm2dev.com>
2018-11-29 02:21:07 -0300

    WIP Allow deploying a previous commit without re-uploading

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var proc = require('child_process');
 var args = require('minimist')(process.argv.slice(2), {
     string: 'config',
     boolean: 'force',
+    string: 'commit',
     default: {
         config: 'config.json',
         force: false
@@ -34,7 +35,7 @@ var status = proc.execSync('git status').toString().split("\n");
 
 var nothingToCommit = status[1].startsWith('nothing to commit');
 
-if(!nothingToCommit && !args.force) {
+if(!nothingToCommit && !args.force && !args.commit) {
     console.log('There are pending changes');
     return false;
 }
@@ -49,105 +50,169 @@ var hash = proc.execSync('git rev-parse --short HEAD').toString().trim();
 
 var files = [];
 
-// Walk the given path
-var walker = walk.walk(config.walk.path, {filters: config.walk.filters});
+if(args.commit) {
 
-console.log("Starting upload to " + config.aws.s3.bucket + "/history/" + hash + "\n");
-
-// For each file on the directory...
-walker.on('file', function(root, file, next) {
-
-    var filePath = root + '/' + file.name;
-
-    var uploadTo = 'history/' + hash + '/' + filePath.replace(config.walk.path + "/", "");
-
-    var params = {
-        ACL: 'private',
-        Body: fs.createReadStream(filePath),
+    var listParams = {
         Bucket: config.aws.s3.bucket,
-        Key: uploadTo
+        Prefix: 'history/' + args.commit
     };
 
-    var type = mime.getType(file.name);
+    s3.listObjects(listParams, function(err, data) {
 
-    if(type) {
-        params.ContentType = type;
-    }
+        files = data.Contents.map(content => ({
+            path: config.walk.path + '/' + content.Key.split('/').pop(),
+            uploadTo: content.Key,
+        }));
 
-    console.log('Uploading ' + filePath);
+        s3.listObjects({Bucket: config.aws.s3.bucket}, function(err, data) {
 
-    s3.upload(params, function(err, data) {
+            if(err) {
+                console.log(err);
 
-        if (err) {
-            console.log("Upload failed\n", err.message);
+                return false;
+            }
 
-            return false;
-        }
+            var objects = [];
 
-        console.log("File uploaded!\n");
+            data.Contents.forEach(function(content) {
 
-        files.push({
-            'path': filePath,
-            'uploadTo': uploadTo
-        });
+                if(
+                    !content.Key.startsWith('history/') &&
+                    !helpers.ignored(content.Key, config.keep)
+                ) {
+                    objects.push({Key: content.Key});
+                }
+            });
 
-        next();
-    });
-});
+            if(objects.length > 0) {
 
-// After uploading all files.
-walker.on('end', function() {
+                console.log('Deleting previous deploy...');
 
-    if(files.length < 1) {
-        console.log('Nothing to upload');
-        return false;
-    }
+                var params = {
+                    Bucket: config.aws.s3.bucket,
+                    Delete: { Objects: objects }
+                };
 
-    console.log('Finished uploading ' + files.length + " file(s)\n");
+                s3.deleteObjects(params, function(err, data) {
 
-    s3.listObjects({Bucket: config.aws.s3.bucket}, function(err, data) {
+                    // If deleting failed
+                    if (err) {
+                        console.log(err);
 
-        if(err) {
-            console.log(err);
+                        return false;
+                    }
 
-            return false;
-        }
-
-        var objects = [];
-
-        data.Contents.forEach(function(content) {
-
-            if(
-                !content.Key.startsWith('history/') &&
-                !helpers.ignored(content.Key, config.keep)
-            ) {
-                objects.push({Key: content.Key});
+                    console.log("Finished deleting previous deploy\n");
+                    helpers.deploy(files, config, s3, aws);
+                });
+            } else {
+                helpers.deploy(files, config, s3, aws);
             }
         });
-
-        if(objects.length > 0) {
-
-            console.log('Deleting previous deploy...');
-
-            var params = {
-                Bucket: config.aws.s3.bucket,
-                Delete: { Objects: objects }
-            };
-
-            s3.deleteObjects(params, function(err, data) {
-
-                // If deleting failed
-                if (err) {
-                    console.log(err);
-
-                    return false;
-                }
-
-                console.log("Finished deleting previous deploy\n");
-                helpers.deploy(files, config, s3, aws);
-            });
-        } else {
-            helpers.deploy(files, config, s3, aws);
-        }
     });
-});
+}
+
+else {
+    // Walk the given path
+    var walker = walk.walk(config.walk.path, {filters: config.walk.filters});
+
+    console.log("Starting upload to " + config.aws.s3.bucket + "/history/" + hash + "\n");
+
+    // For each file on the directory...
+    walker.on('file', function(root, file, next) {
+
+        var filePath = root + '/' + file.name;
+
+        var uploadTo = 'history/' + hash + '/' + filePath.replace(config.walk.path + "/", "");
+
+        var params = {
+            ACL: 'private',
+            Body: fs.createReadStream(filePath),
+            Bucket: config.aws.s3.bucket,
+            Key: uploadTo
+        };
+
+        var type = mime.getType(file.name);
+
+        if(type) {
+            params.ContentType = type;
+        }
+
+        console.log('Uploading ' + filePath);
+
+        s3.upload(params, function(err, data) {
+
+            if (err) {
+                console.log("Upload failed\n", err.message);
+
+                return false;
+            }
+
+            console.log("File uploaded!\n");
+
+            files.push({
+                'path': filePath,
+                'uploadTo': uploadTo
+            });
+
+            next();
+        });
+    });
+
+    // After uploading all files.
+    walker.on('end', function() {
+
+        if(files.length < 1) {
+            console.log('Nothing to upload');
+            return false;
+        }
+
+        console.log('Finished uploading ' + files.length + " file(s)\n");
+
+        s3.listObjects({Bucket: config.aws.s3.bucket}, function(err, data) {
+
+            if(err) {
+                console.log(err);
+
+                return false;
+            }
+
+            var objects = [];
+
+            data.Contents.forEach(function(content) {
+
+                if(
+                    !content.Key.startsWith('history/') &&
+                    !helpers.ignored(content.Key, config.keep)
+                ) {
+                    objects.push({Key: content.Key});
+                }
+            });
+
+            if(objects.length > 0) {
+
+                console.log('Deleting previous deploy...');
+
+                var params = {
+                    Bucket: config.aws.s3.bucket,
+                    Delete: { Objects: objects }
+                };
+
+                s3.deleteObjects(params, function(err, data) {
+
+                    // If deleting failed
+                    if (err) {
+                        console.log(err);
+
+                        return false;
+                    }
+
+                    console.log("Finished deleting previous deploy\n");
+                    helpers.deploy(files, config, s3, aws);
+                });
+            } else {
+                helpers.deploy(files, config, s3, aws);
+            }
+        });
+    });
+}
